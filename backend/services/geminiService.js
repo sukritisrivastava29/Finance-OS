92c784150b28d1b6c6a8d7b4f758a93a4f156cd8
Sukriti Srivastava <sukriti.srivastava2903@gmail.com>
2026-06-29 22:58:58 +0530

    basic chatbot frontend

---
+++ backend/services/geminiService.js
@@ -0,0 +1,21 @@
+const { GoogleGenAI } = require("@google/genai");
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+const askGemini = async (prompt) => {
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+    });
+
+    return response.text;
+  } catch (error) {
+    console.error("Gemini Error:", error);
+    throw error;
+  }
+};
+
+module.exports = askGemini;
