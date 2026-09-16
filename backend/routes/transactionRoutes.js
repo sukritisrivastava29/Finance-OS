9a27195cc1ca6738a58e063aca3c9457a4104660
Sukriti Srivastava <sukriti.srivastava2903@gmail.com>
2026-09-16 11:28:03 +0530

    feat: set up for settings functionality in transactions

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -3,21 +3,24 @@ const express = require("express");
 const {
   createTransaction,
   getTransactions,
-   deleteTransaction,
-   getSummary,
-   updateTransaction,
-   getAnalytics,
+  deleteTransaction,
+  clearTransactions,
+  getSummary,
+  updateTransaction,
+  getAnalytics,
 } = require("../controllers/transactionController");
 
 const router = express.Router();
 const protect = require("../middleware/authMiddleware");
-router.post("/", protect, createTransaction);
 
+router.post("/", protect, createTransaction);
 router.get("/", protect, getTransactions);
 router.get("/summary", protect, getSummary);
 router.get("/analytics", protect, getAnalytics);
 
+router.delete("/", protect, clearTransactions);
 router.delete("/:id", protect, deleteTransaction);
+
 router.put("/:id", protect, updateTransaction);
 
 module.exports = router;
